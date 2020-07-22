6e6962fe4dfdd7177df49c9e3710dedace33247d
ruhita <68579730+ruhita@users.noreply.github.com>
2020-07-22 10:51:25 +0530

    assignment2

---
+++ Day 2-LU .py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Arithmetic operators
+
+   **, *, /, %, +, -, //
+
+
+# In[2]:
+
+
+x = 25
+y = 8
+
+
+print('x + y =',x+y)
+
+
+print('x - y =',x-y)
+
+
+print('x * y =',x*y)
+
+
+print('x / y =',x/y)
+
+
+print('x // y =',x//y)
+
+
+print('x ** y =',x**y)
+
+
+# In[ ]:
+
+
+Comparision  operators
+
+    ==, <, >, <=, >=, !=
+
+
+# In[3]:
+
+
+x = 25
+y = 8
+
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+
+
+# In[ ]:
+
+
+Assignment operators 
+
+    =, +=, -=, *=, /=, **=
+forexample
+x=8
+x=x+8
+x=x-8
+
+
+# In[ ]:
+
+
+Logical operators
+
+   AND, OR, NOT
+
+
+# In[4]:
+
+
+x = True
+y = False
+
+print('x and y is',x and y)
+
+print('x or y is',x or y)
+
+print('not x is',not x)
+
+
+# In[ ]:
+
+
+Variables defination rules
+variables should start with a character. 
+Variable can lower or upper case, numbers, underscore.   
+Example , a=25, A=25, -=25
+here a and A are two different variables
+
